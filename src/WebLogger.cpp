bf90c6decee87c4c1c7add4e8d881117d6dd4424
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2020-02-03 15:24:59 +0100

    Logger: shows the timestamp

--- src/WebLogger.cpp
+++ src/WebLogger.cpp
@@ -10,23 +10,30 @@
 #include "WebParameters.h"
 #include "WebLogger.h"
 
+
+void formatUrl (std::string & s, const char * url);
+
+
 void WebLogger::logUrl (const char * url)
 {
-	this->sendToLog (url);
+	std::string s;
+	formatUrl (s, url);
+
+	this->sendToLog (s.c_str ());
 }
 
 void WebLogger::logParams (const char * url, WebParameters & requestParams)
 {
-	std::string s (url);
-	s.append ("\n");
+	std::string s;
+	formatUrl (s, url);
 
 
 	WebParametersPrivateData & wpPd = requestParams.getPrivateData ();
 
 	for (auto node : wpPd.parameters)
 	{
-		s.append ("\t");
-		s.append (node.first).append ("\n");
+		s.append ("\n\t");
+		s.append (node.first);
 	}
 
 	this->sendToLog (s.c_str ());
@@ -35,14 +42,15 @@ void WebLogger::logParams (const char * url, WebParameters & requestParams)
 
 void WebLogger::logParamsContents (const char * url, WebParameters & requestParams)
 {
-	std::string s (url);
-	s.append ("\n");
+
+	std::string s;
+	formatUrl (s, url);
 
 	WebParametersPrivateData & wpPd = requestParams.getPrivateData ();
 
 	for (auto node : wpPd.parameters)
 	{
-		s.append ("\t");
+		s.append ("\n\t");
 		Parameter & p = node.second;
 		s.append (node.first).append (" = ");
 		if (p.isSet)
@@ -53,8 +61,28 @@ void WebLogger::logParamsContents (const char * url, WebParameters & requestPara
 		{
 			s.append ("(default)").append (p.defaultValue);
 		}
-		s.append ("\n");
 	}
 
 	this->sendToLog (s.c_str ());
+}
+
+
+#include <chrono>  // chrono::system_clock
+#include <ctime>   // localtime
+
+
+void formatUrl (std::string & s, const char * url)
+{
+	auto now = std::chrono::system_clock::now ();
+	auto in_time_t = std::chrono::system_clock::to_time_t (now);
+
+
+	struct tm buf;
+	gmtime_s (&buf, &in_time_t);
+
+
+	char str[26];
+	strftime ((char *)str, sizeof (str), "%F %T UTC", &buf);
+
+	s.append ("[").append (str).append ("]\t").append (url);
 }
