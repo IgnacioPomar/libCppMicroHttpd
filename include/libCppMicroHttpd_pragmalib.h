882adf3da9a8de84f5fb5639b863d5aa5432cc63
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2024-02-07 16:03:43 +0100

    Fix documentation

--- include/libCppMicroHttpd_pragmalib.h
+++ include/libCppMicroHttpd_pragmalib.h
@@ -7,18 +7,23 @@
 #ifndef _PRAGMA_LIB_LIB_CPP_MICRO_HTTPD_H_
 #define _PRAGMA_LIB_LIB_CPP_MICRO_HTTPD_H_
 
-/*
-#ifdef _DEBUG
-#pragma comment(lib, "libmicrohttpd-dll_d.lib")
-#else
-#pragma comment(lib, "libmicrohttpd-dll.lib")
-#endif
-*/
+//true to use static, false to use dll	
+#if false
+
 #ifdef _DEBUG 
 #pragma comment(lib, "libmicrohttpd_d.lib")
 #else
 #pragma comment(lib, "libmicrohttpd.lib")
 #endif
 
+#else
+
+#ifdef _DEBUG
+#pragma comment(lib, "libmicrohttpd-dll_d.lib")
+#else
+#pragma comment(lib, "libmicrohttpd-dll.lib")
+#endif
+
+#endif
 
 #endif // _PRAGMA_LIB_LIB_CPP_MICRO_HTTPD_H_
